5d1b5167e5601925b9d9284c44f14ce7a652b752
Boris Labianca <boris.labianca@mbp-de-boris-1.home>
2022-11-02 15:34:06 +0100

    final modifications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, modifyCounter] = useState(0);
+  // const [counter, removeFromCounter] = useState(0);
+  // const [counter, resetCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <i class="fa-solid fa-calculator"></i> <span>React Counter</span>
       </header>
+      <main>
+        <div className="counter-container">
+          <div className={counter === 0 && "hidden"}>
+            <i
+              onClick={() => {
+                modifyCounter(counter - 1);
+              }}
+              class="fa-solid fa-minus"
+            ></i>
+          </div>
+
+          <div className="counter">{counter}</div>
+          <div className={counter === 10 && "hidden"}>
+            <i
+              onClick={() => {
+                modifyCounter(counter + 1);
+              }}
+              class="fa-solid fa-plus"
+            ></i>
+          </div>
+        </div>
+        <div className="button-div">
+          <button
+            onClick={() => {
+              modifyCounter(0);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </main>
+      <footer>
+        Made with{" "}
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+          React
+        </a>{" "}
+        at{" "}
+        <a href="https://www.lereacteur.io/" target="_blank" rel="noreferrer">
+          Le Reacteur
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/boris-labianca-01a52871/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Boris
+        </a>
+      </footer>
     </div>
   );
 }
